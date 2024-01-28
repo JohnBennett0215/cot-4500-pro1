45ba562ba2c47eed4be9b795c66ad1e2ea43bd85
JohnBennett0215 <156384711+JohnBennett0215@users.noreply.github.com>
2024-01-27 20:31:11 -0500

    Add files via upload

---
+++ project2.py
@@ -0,0 +1,90 @@
+def f(x):
+    return pow(x, 3) + 4 * pow(x, 2) - 10
+    
+def fp(x):
+    return 3 * pow(x, 2) + 8 * x
+   
+def g(x):
+    return pow(3, -x)
+
+tol = 0.0001
+left = -4
+right = 7
+
+max = 1000
+i = 0
+
+while(abs(right - left) > tol and i < max):
+    i = i + 1
+    p = (left + right) / 2
+    
+    if (f(left) < 0 and f(p) > 0):
+        right = p
+    else:
+        left = p
+        
+print(i)
+
+p = -4
+pn = 0.0001
+tol = 0.0001
+maxi = 1000
+i = 1
+
+while ( i <= maxi):
+
+    if(fp(p) != 0):
+    
+        pn = p - f(p) / fp(p)
+        
+        if (abs(pn - p) < tol):
+        
+            print(i)
+            
+            print("Success")
+            
+            break
+            
+        i = i + 1
+        
+        p = pn
+        
+    else:
+    
+        print("Unsuccessful")
+         
+
+error_tolerance = 0.000001
+
+guess = 1.0
+i = 0
+
+while True:
+    next_guess = 0.5 * (guess + 5 / guess)
+    
+    print(f"iteration {i + 1}: {next_guess}")
+    
+    if abs(next_guess - guess) < error_tolerance:
+        break
+        
+    guess = next_guess
+    i += 1
+    
+print(f"\nApproximated square root of 5: {next_guess}")
+print(f"Number of iterations: {i + 1}")
+
+
+pn = 1
+tol = 0.000001
+n = 1000
+
+
+i = 1
+while (i <= n):
+   p = g(p)
+   if (abs(p - pn) < tol):
+        print(p, "Success")
+        break
+        
+   i = i + 1
+   pn = p
